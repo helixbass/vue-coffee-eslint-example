0399fdbcb122b3d70330e372db2b8b1c0c158bc9
Julian Rosse <julian@helixbass.net>
2020-01-03 15:40:44 -0700

    <script lang="coffee">

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,13 +34,12 @@
   </div>
 </template>
 
-<script>
-export default {
+<script lang="coffee">
+export default
   name: 'HelloWorld',
   props: {
     msg: String
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
